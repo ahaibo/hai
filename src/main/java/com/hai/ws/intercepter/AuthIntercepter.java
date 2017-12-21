a539055a6b09e313b470efa446534c70ee404175
ahaibo <785155409@qq.com>
2017-12-21 22:02:29 +0800

    web service for cxf demo practise.

--- src/main/java/com/hai/ws/intercepter/AuthIntercepter.java
+++ src/main/java/com/hai/ws/intercepter/AuthIntercepter.java
@@ -6,6 +6,7 @@ import org.apache.cxf.headers.Header;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.springframework.stereotype.Component;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
@@ -15,6 +16,7 @@ import javax.xml.namespace.QName;
  * cxf 拦截器：权限拦截
  * Created by Administrator on 2017/12/14.
  */
+@Component
 public class AuthIntercepter extends AbstractPhaseInterceptor<SoapMessage> {
 
     public AuthIntercepter() {
@@ -22,7 +24,7 @@ public class AuthIntercepter extends AbstractPhaseInterceptor<SoapMessage> {
     }
 
     public AuthIntercepter(String phase) {
-        super(phase);//在调用方法之前调用拦截器
+        super(phase);//调用拦截器的阶段
         System.out.println(this.getClass().getName() + ".phase: " + phase);
     }
 
@@ -39,19 +41,21 @@ public class AuthIntercepter extends AbstractPhaseInterceptor<SoapMessage> {
         NodeList usernameNodeList = element.getElementsByTagName("username");
         NodeList passwordNodeList = element.getElementsByTagName("password");
         if (null == usernameNodeList || usernameNodeList.getLength() != 1) {
-            new Fault(new IllegalArgumentException("error format for username"));
+            throw new Fault(new IllegalArgumentException("error format for username"));
         }
         if (null == passwordNodeList || passwordNodeList.getLength() != 1) {
-            new Fault(new IllegalArgumentException("error format for password"));
+            throw new Fault(new IllegalArgumentException("error format for password"));
         }
 
         String username = usernameNodeList.item(0).getTextContent();
         String password = passwordNodeList.item(0).getTextContent();
+        System.out.println(this.getClass().getName() + " username: " + username + ";\tpassword: " + password);
+
         if (null == username || username.trim().length() == 0 || !username.equals("hai")) {
-            new Fault(new IllegalArgumentException("error username"));
+            throw new Fault(new IllegalArgumentException("error username"));
         }
         if (null == password || password.trim().length() == 0 || !password.equals("123456")) {
-            new Fault(new IllegalArgumentException("error password"));
+            throw new Fault(new IllegalArgumentException("error password"));
         }
         System.out.println(this.getClass().getName() + ".auth success...");
     }
--- src/main/java/com/hai/ws/service/HelloWorldImpl.java
+++ src/main/java/com/hai/ws/service/HelloWorldImpl.java
@@ -3,6 +3,7 @@ package com.hai.ws.service;
 import com.hai.ws.api.IHelloWorld;
 import com.hai.ws.entity.Role;
 import com.hai.ws.entity.User;
+import org.springframework.stereotype.Component;
 
 import javax.jws.WebService;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Map;
 /**
  * Created by Administrator on 2017/12/13.
  */
+@Component("helloWorld")
 @WebService
 public class HelloWorldImpl implements IHelloWorld {
     @Override
